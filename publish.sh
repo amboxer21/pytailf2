d9b2ca6ea4f6683fa8fb3758ddebc5b708c1efec
Anthony Guevara <amboxer21@gmail.com>
2019-03-08 16:07:44 -0500

    Delete publish.sh

--- publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo Did you update version?
-read
-./setup.py sdist --formats=bztar upload
-#echo "Generating docs"
-#./setup.py build_sphinx
-#echo "Uploading docs to pypi"
-#./setup.py upload_docs --upload-dir doc/_build/html/
